8a2ca0f041e7ab42bfee7882035a576659c2654c
Dimitri C <couwenberg@gmail.com>
2019-05-17 17:39:59 +1000

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Flex, Box } from '@rebass/grid'
+
+const Bar = () => <Box><hr/></Box>
+
+const Heading = ({children}) =>  <Box>{children}</Box>
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<Flex flexWrap=''>
+  <Box p={3} width={[ 1, 1/2 ]}>
+    <Bar />
+    <Heading my={4} fontSize={[ 5, 6, 7 ]}>fbsjkafksakfhsakdfhksadjhfjksdhfjkshdjkfhdsjkhfdsjkhfkjh</Heading>
+  </Box>
+  <Box p={3} width={[ 1, 1/2 ]}>
+    <Bar />
+    <Heading my={4} fontSize={[ 5, 6, 7 ]}>Flexible</Heading>
+  </Box>
+  <Box p={3} width={[ 1, 1/2 ]}>
+    <Bar />
+    <Heading my={4} fontSize={[ 5, 6, 7 ]}>Flexible</Heading>
+  </Box>
+</Flex>
   );
 }
 
